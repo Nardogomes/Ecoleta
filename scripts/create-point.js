4505f93a4d1ba554ab5d1935e43ba7eb9affd30b
Ednardo Gomes <ednardo@email.com>
2020-06-06 18:06:28 -0300

    Criando a lógica para armazenar o ítens selecionados em uma const para enviar no formulário.

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -47,6 +47,10 @@ for(const item of itemsToCollect) {
     item.addEventListener("click", handleSelectedItem)
 }
 
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
 function handleSelectedItem(event) {
     const itemLi = event.target
 
@@ -54,4 +58,26 @@ function handleSelectedItem(event) {
     itemLi.classList.toggle("selected")
 
     const itemId = itemLi.dataset.id
+
+    // Verificar se existe ítens selecionados,
+    // se sim pegar os ítens selecinados.
+    const alreadySelected = selectedItems.findIndex( item => {
+        const itemFound = item == itemId
+        return itemFound
+    })
+    
+    // Caso já esteja selecionado, tirar da seleção
+    if(alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+        selectedItems = filteredItems
+    } else{
+        // Caso não esteja selecionado, adicionar à seleção.
+        selectedItems.push(itemId)
+    }
+    
+    // Atualizar o input escondido com os ítens selecionads.
+    collectedItems.value = selectedItems
 }
