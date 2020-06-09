2de52a7af45430899767efb8ae1d4fc820461dd5
Ednardo Gomes <ednardo@email.com>
2020-06-09 20:31:11 -0300

    Refatorando estrutura do projeto.
    - Instalando a template engine nunjucks
    - Configurando as rotas entre as páaginas
    - Criando diretório partials
    - Separando o código do modal e header em novos arquivos html
    - Colocando os arquivos modal.hrml e page-header.html no diretório
    partials

--- src/server.js
+++ src/server.js
@@ -1,10 +1,28 @@
 const express = require("express")
 const server = express()
 
+// Configurar pasta pública
+server.use(express.static("public"))
+
+// Utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 // Configurando rotas.
 // Página inicial
 server.get("/", (req, res) => {
-    return res.sendFile("../views/index.html")
+    return res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
 })
 
 // Iniciar o servidor
